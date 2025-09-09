09a70d749fc94a9eae1680a86a0d2e1b447f708d
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2025-09-09 16:55:26 +0100

    mypy

--- generate.py
+++ generate.py
@@ -1,7 +1,8 @@
 import math
-import sympy
+import sympy  # type: ignore
 import os
 from arctans import Arctan, Zero, generate
+from arctans.arctans import AbstractTerm
 from machin.formulae import load_formula
 from machin import settings
 
@@ -9,17 +10,16 @@ known_formulae = []
 
 for file in os.listdir(settings.formulae_path):
     if file.endswith(".pi"):
-        pi = Zero()
+        pi: AbstractTerm = Zero()
         for c, a in load_formula(file[:-3]).terms:
             pi += Arctan(c, 1 / sympy.S(a))
         known_formulae.append(pi)
 
-for f in known_formulae:
-    assert (math.pi - float(f)) < 0.0001
+for formula in known_formulae:
+    assert (math.pi - float(formula)) < 0.0001
 
-new_formulae = [
-    f for f in generate(known_formulae, max_terms=8) if 1 not in f.term_dict
-]
+new_formulae = generate(known_formulae, max_terms=8, max_coefficient_denominator=1)
+new_formulae = [formula for formula in new_formulae if 1 not in formula.term_dict]
 
 pi_n = len(known_formulae) + 1
 
