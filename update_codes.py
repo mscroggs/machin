edbeadb770b16578c552f7942cb196c252d4c37f
Matthew Scroggs <matthew.w.scroggs@gmail.com>
2025-10-01 19:24:00 +0100

    rename formulae

--- update_codes.py
+++ update_codes.py
@@ -4,9 +4,10 @@ import os
 from machin import settings
 from webtools.tools import join
 
-for file in os.listdir(settings.formula_dir):
+for file in os.listdir(settings.formulae_path):
     if file.endswith(".pi"):
         if len(file) - 4 != settings.code_digits:
             assert len(file) - 4 < settings.code_digits
             new_file = file[0] + "0" * (settings.code_digits - len(file) + 4) + file[1:]
-            os.system(f"mv {settings.formula_dir}/{file} {settings.formula_dir}/{new_file}")
+            print(f"renaming {file} to {new_file}")
+            os.system(f"mv {settings.formulae_path}/{file} {settings.formulae_path}/{new_file}")
